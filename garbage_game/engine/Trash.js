4a5e9a451a1b837f62b006f94df8c58bfd91e529
mathieu auclair <mathiass12@hotmail.com>
2018-07-10 20:45:15 -0400

    fix error in usedCofffeCup console

--- garbage_game/engine/Trash.js
+++ garbage_game/engine/Trash.js
@@ -1,5 +1,15 @@
 var spritesheet = document.getElementById('spritesheet');
 
+//Random Sprite
+var sprite = new Sprite(
+	spritesheet, //image
+	new Vector2d(100, 100), //clipping
+	new Vector2d(50, 50),   //size
+	new Vector2d(200, 200), //offset
+	new Vector2d(50, 50)	//strech
+);
+
+
 //Create
 var renderer = new Renderer();
 var gameThread = new GameThread(100);
