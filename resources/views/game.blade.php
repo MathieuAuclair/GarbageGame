e1d142767d09f413f8b9e8627f732db90ed6e5eb
mathieuauclair <mathiass12@hotmail.com>
2018-09-27 09:51:54 -0400

    set laravel to the project

---
+++ resources/views/game.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JOHN! (┛°□°)┛彡┻━┻</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="Ugly.css">
+    <script src="RottenPotatoe.js"></script>
+    <script src="Utils/BurnedTree.js"></script>
+    <script src="Utils/RuinedBook.js"></script>
+    <script src="Utils/FishScale.js"></script>
+    <script src="Utils/BananaPeel.js"></script>
+    <script src="Utils/DirtyBaseballCard.js"></script>
+    <script src="Utils/RustyMetalClock.js"></script>
+    <script src="Collision/JunkYard.js"></script>
+    <script src="Sprite/GarbageBin.js"></script>
+</head>
+<body>
+<div id="mocha"></div>
+<div class="wrapper">
+    <div class="container">
+        <canvas id="canvas" width="900" height="500"></canvas>
+    </div>
+</div>
+<img id="spritesheet" src="Sprite/Templates/plastic-bag.png">
+<img id="texture_grass" src="Sprite/Templates/rotten-grass.jpg">
+<img id="foreground" src="Sprite/Templates/abandoned_shop.png">
+<img id="foreground_decoration" src="Sprite/Templates/rusty-tower.png">
+<img id="foreground_detail" src="Sprite/Templates/broken-stick.png">
+<img id="skybox" src="Sprite/Templates/wrecked-plane.png">
+<img id="car" src="Sprite/Templates/broken-car.png">
+<script src="Controller/DeadMouse.js"></script>
+<script src="Controller/BrokenKeyboard.js"></script>
+<script src="ProjectFiles/EmptyShotgunShell.js"></script>
+<script src="ProjectFiles/RadioActiveMatter.js"></script>
+<script src="ProjectFiles/SmashedGameController.js"></script>
+<script src="ProjectFiles/Rendering/EmptySodaCan.js"></script>
+<script src="ProjectFiles/Rendering/BurnedPencil.js"></script>
+<script src="ProjectFiles/Animator/MeltedMovieTape.js"></script>
+<script src="ProjectFiles/StaticCollider/CrashedCar.js"></script>
+<script src="ProjectFiles/UiScreenDisplay/TornUpLetter.js"></script>
+<script src="Trash.js"></script>
+</body>
+</html>
