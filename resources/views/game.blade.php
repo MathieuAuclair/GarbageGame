cabc96aae35b75367b51efdfca36293f4407d139
mathieuauclair <mathiass12@hotmail.com>
2018-09-27 10:05:35 -0400

    fix route for engine

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -3,16 +3,16 @@
 <head>
     <title>JOHN! (┛°□°)┛彡┻━┻</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="Ugly.css">
-    <script src="RottenPotatoe.js"></script>
-    <script src="Utils/BurnedTree.js"></script>
-    <script src="Utils/RuinedBook.js"></script>
-    <script src="Utils/FishScale.js"></script>
-    <script src="Utils/BananaPeel.js"></script>
-    <script src="Utils/DirtyBaseballCard.js"></script>
-    <script src="Utils/RustyMetalClock.js"></script>
-    <script src="Collision/JunkYard.js"></script>
-    <script src="Sprite/GarbageBin.js"></script>
+    <link rel="stylesheet" type="text/css" href="/engine/Ugly.css">
+    <script src="/engine/RottenPotatoe.js"></script>
+    <script src="/engine/Utils/BurnedTree.js"></script>
+    <script src="/engine/Utils/RuinedBook.js"></script>
+    <script src="/engine/Utils/FishScale.js"></script>
+    <script src="/engine/Utils/BananaPeel.js"></script>
+    <script src="/engine/Utils/DirtyBaseballCard.js"></script>
+    <script src="/engine/Utils/RustyMetalClock.js"></script>
+    <script src="/engine/Collision/JunkYard.js"></script>
+    <script src="/engine/Sprite/GarbageBin.js"></script>
 </head>
 <body>
 <div id="mocha"></div>
@@ -21,23 +21,23 @@
         <canvas id="canvas" width="900" height="500"></canvas>
     </div>
 </div>
-<img id="spritesheet" src="Sprite/Templates/plastic-bag.png">
-<img id="texture_grass" src="Sprite/Templates/rotten-grass.jpg">
-<img id="foreground" src="Sprite/Templates/abandoned_shop.png">
-<img id="foreground_decoration" src="Sprite/Templates/rusty-tower.png">
-<img id="foreground_detail" src="Sprite/Templates/broken-stick.png">
-<img id="skybox" src="Sprite/Templates/wrecked-plane.png">
-<img id="car" src="Sprite/Templates/broken-car.png">
-<script src="Controller/DeadMouse.js"></script>
-<script src="Controller/BrokenKeyboard.js"></script>
-<script src="ProjectFiles/EmptyShotgunShell.js"></script>
-<script src="ProjectFiles/RadioActiveMatter.js"></script>
-<script src="ProjectFiles/SmashedGameController.js"></script>
-<script src="ProjectFiles/Rendering/EmptySodaCan.js"></script>
-<script src="ProjectFiles/Rendering/BurnedPencil.js"></script>
-<script src="ProjectFiles/Animator/MeltedMovieTape.js"></script>
-<script src="ProjectFiles/StaticCollider/CrashedCar.js"></script>
-<script src="ProjectFiles/UiScreenDisplay/TornUpLetter.js"></script>
-<script src="Trash.js"></script>
+<img id="spritesheet" src="/engine/Sprite/Templates/plastic-bag.png">
+<img id="texture_grass" src="/engine/Sprite/Templates/rotten-grass.jpg">
+<img id="foreground" src="/engine/Sprite/Templates/abandoned_shop.png">
+<img id="foreground_decoration" src="/engine/Sprite/Templates/rusty-tower.png">
+<img id="foreground_detail" src="/engine/Sprite/Templates/broken-stick.png">
+<img id="skybox" src="/engine/Sprite/Templates/wrecked-plane.png">
+<img id="car" src="/engine/Sprite/Templates/broken-car.png">
+<script src="/engine/Controller/DeadMouse.js"></script>
+<script src="/engine/Controller/BrokenKeyboard.js"></script>
+<script src="/engine/ProjectFiles/EmptyShotgunShell.js"></script>
+<script src="/engine/ProjectFiles/RadioActiveMatter.js"></script>
+<script src="/engine/ProjectFiles/SmashedGameController.js"></script>
+<script src="/engine/ProjectFiles/Rendering/EmptySodaCan.js"></script>
+<script src="/engine/ProjectFiles/Rendering/BurnedPencil.js"></script>
+<script src="/engine/ProjectFiles/Animator/MeltedMovieTape.js"></script>
+<script src="/engine/ProjectFiles/StaticCollider/CrashedCar.js"></script>
+<script src="/engine/ProjectFiles/UiScreenDisplay/TornUpLetter.js"></script>
+<script src="/engine/Trash.js"></script>
 </body>
 </html>
